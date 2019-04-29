3f8b82380984772248e824e83b4750fda2082a0d
Julio Rovira <julio.rovirato@amigo.edu.co>
2019-04-29 08:23:50 -0500

    Version1.23

---
+++ SistemaAC/Controllers/ReportesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaAC.Controllers
+{
+    public class ReportesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
